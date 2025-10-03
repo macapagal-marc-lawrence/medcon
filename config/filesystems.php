c22c54158382206f3fd43212a3951bd328d54823
macapagal-marc-lawrence <macapagalmarck@gmail.com>
2025-10-03 09:11:25 +0800

    upload image edited

--- config/filesystems.php
+++ config/filesystems.php
@@ -60,6 +60,16 @@ return [
             'report' => false,
         ],
 
+        'public_uploads' => [
+            'driver' => 'local',
+            'root' => public_path('uploads'),
+            'url' => env('APP_URL') . '/uploads',
+            'visibility' => 'public',
+            'throw' => false,
+            'report' => false,
+],
+
+
     ],
 
     /*
